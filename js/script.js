2621851de100b731af5430933278d217693dd741
Ramon Bolivar <bolivarcampos@gmail.com>
2023-05-02 14:59:53 +0100

    agrego js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let nombre = prompt("Por favor, ingrese su nombre:");
+let talla = prompt("Cuál es tu talla?");
+alert("¡Bienvenido a Frontend Store tu tienda de Remeras IT " + nombre + "!");
+alert("Tenemos en stock varios modelos que se adaptan a ti");
