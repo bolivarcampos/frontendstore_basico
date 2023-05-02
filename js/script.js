451e963ea04ec34850cdd47532a1022a949e8ed6
Ramon Bolivar <bolivarcampos@gmail.com>
2023-05-02 17:09:40 +0100

    Más JS

--- js/script.js
+++ js/script.js
@@ -2,3 +2,31 @@ let nombre = prompt("Por favor, ingrese su nombre:");
 let talla = prompt("Cuál es tu talla?");
 alert("¡Bienvenido a Frontend Store tu tienda de Remeras IT " + nombre + "!");
 alert("Tenemos en stock varios modelos que se adaptan a ti");
+
+let lenguaje = prompt(
+  "¿Cuál es tu lenguaje de programación preferido? JavaScript, Java, Python, C, C++"
+);
+
+switch (lenguaje) {
+  case "JavaScript":
+    alert("Excelente lenguaje usado para Front y Backend");
+    break;
+  case "Java":
+    alert("Lenguaje Orientada a Objeto");
+    break;
+  case "Python":
+    alert("Lenguaje Fácil y Eficiente de aprender, muy usado actualmente");
+    break;
+  case "C":
+    alert("Lenguaje más complejo de aprender y de bajo nivel!");
+    break;
+  case "C++":
+    alert(
+      "Como C es complejo de aprender pero muy vérsatil y dinámico, muy demandado"
+    );
+    break;
+
+  default:
+    alert("¡No se ha podido determinar tu lenguaje de programación!");
+    break;
+}
